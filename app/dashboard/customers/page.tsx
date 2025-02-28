cca07aa1724f62fffd40c86ef0b3eef57552c1f4
sb <sb332sb@gmail.com>
2025-02-28 15:23:08 +0100

    firstproject

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function customer(){
+    return(
+        <p>IT is for customers</p>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function dashboard(){
+    return(
+        <p>IT is a Dashboard</p>
+    )
+}
